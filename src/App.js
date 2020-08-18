b443efe45ca93bae80bff2f2d8a2e73bd39c7b07
Matheus Cordeiro <matheusdebarroscordeiro@gmail.com>
2020-08-18 12:40:56 -0300

    Components

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
 import React from "react";
 import GlobalStyle from "./styles/global";
+import { Header } from "./components/Header";
 
 function App() {
-  <GlobalStyle />;
+  <>
+    <Header />
+    <GlobalStyle />;
+  </>;
 }
 
 export default App;
